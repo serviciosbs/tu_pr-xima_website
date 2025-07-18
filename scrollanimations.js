3a57b7777862026bd7a1712c515045d5ce8e3f4a
serviciosbs <bs.services.mx@gmail.com>
2025-07-17 20:15:48 -0600

    Create scrollanimations.js

---
+++ scrollanimations.js
@@ -0,0 +1,31 @@
+// scrollAnimations.js
+ScrollReveal().reveal(".hero", {
+  duration: 1000,
+  origin: "top",
+  distance: "50px",
+});
+ScrollReveal().reveal(".story", {
+  duration: 1000,
+  origin: "left",
+  distance: "50px",
+});
+ScrollReveal().reveal(".menu", {
+  duration: 1000,
+  origin: "right",
+  distance: "50px",
+});
+ScrollReveal().reveal(".weekly-promos", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "50px",
+});
+ScrollReveal().reveal(".events", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "50px",
+});
+ScrollReveal().reveal(".socials", {
+  duration: 1000,
+  origin: "bottom",
+  distance: "50px",
+});
